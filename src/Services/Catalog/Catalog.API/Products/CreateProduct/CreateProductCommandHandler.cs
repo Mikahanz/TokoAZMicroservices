02a288341b4b3be097722c7660e5c71eb1ece0f1
Michael Hanzel <mhanzel@tradefan.com>
2025-01-07 21:55:40 -0500

    Create Abstraction on MediatR for CQRS - Command & Query. Implement CQRS Abstraction ICommand into Slice Feature. Create BuildingBlocks for CQRS using MediatR.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,31 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, decimal Price, string ImageFile)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create product object from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Category = command.Category,
+            Price = command.Price
+        };
+        
+        // Save to Database
+        // Return CreateProductResult result
+        
+        return Task.FromResult(new CreateProductResult(product.Id));
+    }
+}
