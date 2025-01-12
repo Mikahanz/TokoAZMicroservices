0554f8552ddc92d7ffeeb655a2f10fca831fe0f7
Michael Hanzel <mhanzel@tradefan.com>
2025-01-11 21:34:47 -0500

    Implement validators to DeleteProductCommandHandler.cs & UpdateProductCommandHandler.cs

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -8,6 +8,16 @@ public record UpdateProductCommand(Guid Id,
                                     decimal Price) : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is requred");
+        RuleFor(command => command.Name).NotEmpty().WithMessage("Name is required")
+                                        .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+    }
+}
+
 internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
