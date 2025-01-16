33bd996d460a2487bb18275bd9e3904c44a14d5f
Michael Hanzel <mhanzel@tradefan.com>
2025-01-16 11:21:33 -0500

    Remove appSettings.json from .gitignore. implement IBasketRepository in GetBasketQueryHandler.cs

--- src/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
+++ src/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -1,13 +1,15 @@
+using Basket.API.Data;
+
 namespace Basket.API.Basket.GetBasket;
 
 public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
 public record GetBasketResult(ShoppingCart Cart);
 
-internal class GetBasketQueryHandler(IDocumentSession session) : IQueryHandler<GetBasketQuery, GetBasketResult>
+internal class GetBasketQueryHandler(IBasketRepository basketRepository) : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
     public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
-        // var shoppingCard = await session.LoadAsync<ShoppingCart>(query.UserName);
-        return new GetBasketResult(new ShoppingCart("test"));
+        var basket = await basketRepository.GetBasket(query.UserName, cancellationToken);
+        return new GetBasketResult(basket);
     }
 }
