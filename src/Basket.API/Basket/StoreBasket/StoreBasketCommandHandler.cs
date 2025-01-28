55f7fd5831f5576c491db7e7efae544b07f4bbe5
Michael Hanzel <mhanzel@tradefan.com>
2025-01-27 21:48:20 -0500

    Setup Basket.API.csproj as bRPC client and implement discount in StoreBasketCommandHandler

--- src/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,3 +1,5 @@
+using Discount.gRPC;
+
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -12,15 +14,25 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-internal class StoreBasketCommandHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+internal class StoreBasketCommandHandler(IBasketRepository basketRepository, 
+                                        DiscountProtoService.DiscountProtoServiceClient discountProtoService) 
+                                                    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        ShoppingCart cart = command.Cart;
+        await DeductDiscount(command.Cart, cancellationToken);
         
-        await basketRepository.StoreBasket(cart, cancellationToken);
-        // To Do: Update cache (use Redis)
+        await basketRepository.StoreBasket(command.Cart, cancellationToken);
         
         return new StoreBasketResult(command.Cart.UserName);
+    }
+    
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProtoService.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
     }
 }
