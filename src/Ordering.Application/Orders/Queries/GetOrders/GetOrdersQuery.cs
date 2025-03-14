73df585099764492a424fad751be9a36d37ec136
Michael Hanzel <mhanzel@tradefan.com>
2025-03-14 13:29:03 -0400

    Create GetOrdersQuery & GetOrdersQueryHandler. Implement Pagination.

---
+++ src/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,9 @@
+using BuildingBlocks.CQRS;
+using BuildingBlocks.Pagination;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersQueryResult>;
+
+public record GetOrdersQueryResult(PaginatedResult<OrderDto> Orders);
