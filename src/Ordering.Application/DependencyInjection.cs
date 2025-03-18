fab6e4dc61a7b7a9b8215da47d889ebd9b069ab0
AzureAD\MichaelHanzel <mhanzel@tradedan.com>
2025-03-18 12:00:46 -0400

    Register MassTransit RabbitMQ packages into Ordering.Application DI

--- src/Ordering.Application/DependencyInjection.cs
+++ src/Ordering.Application/DependencyInjection.cs
@@ -1,18 +1,24 @@
 ﻿using System.Reflection;
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
+        // Add MediatR
         services.AddMediatR(config => {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             config.AddOpenBehavior(typeof(ValidationBehavior<,>));
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
+        
+        // Add message broker
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
         return services;
     }
